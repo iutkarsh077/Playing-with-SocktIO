ba184ca65415bd6516a9eac8a5ce1f82a2a78334
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-11-16 14:07:02 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotnenv from "dotenv";
 import authRoutes from "./Routes/auth.routes.js";
@@ -8,16 +9,24 @@ import cookieParser from "cookie-parser";
 import { server, app } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotnenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.send("Hello from backend");
-});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from backend");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
